b929bd63019d91c1eef0a7e0dc71b94e52fa872e
anjali906 <lohita59@yahoo.com>
2021-02-27 16:08:42 +0530

    Added services to application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { SearchService } from './search.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SpotifyMusicApp';
+  constructor(private authService: SearchService){}
+
+  ngOnInit(){
+    // this.authService.authorize().subscribe(data => console.log(data));
+  }
 }
